290c8e80eabe00207579b28e0ee1049b68f28e7e
krishnaluharuka <104828115+krishnaluharuka@users.noreply.github.com>
2024-06-05 15:12:24 +0545

    Update view_products.php

--- Hi5treasures_recomm/admin_area/view_products.php
+++ Hi5treasures_recomm/admin_area/view_products.php
@@ -16,6 +16,7 @@ if (!isset($admin_user)) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.min.css" />
     <style>
         .bg {
             background-color: antiquewhite;
@@ -106,9 +107,16 @@ if (!isset($admin_user)) {
 
         </tbody>
     </table>
-    <?php 
-include ('page.php');
-?>
+   <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <script>
+  $(document).ready( function () {
+    $('#myTable').DataTable();
+  });
+</script>
 </body>
 
 </html>
