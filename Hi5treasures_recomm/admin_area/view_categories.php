be16d6aedd93a9c341c5dcbd7ded49076c20e79c
krishnaluharuka <104828115+krishnaluharuka@users.noreply.github.com>
2024-06-05 15:11:28 +0545

    Update view_categories.php

--- Hi5treasures_recomm/admin_area/view_categories.php
+++ Hi5treasures_recomm/admin_area/view_categories.php
@@ -81,8 +81,13 @@ if (!isset($admin_user)) {
 <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-
-    <!-- Modal -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+
+    <script>
+  $(document).ready( function () {
+    $('#myTable').DataTable();
+  });
+</script>
 
 </body>
 
